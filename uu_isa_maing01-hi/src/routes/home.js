7b91d7c53d02aae4930a314caab5af625585bdef
Patrik Basista <patrik.basista@plus4u.net>
2020-10-25 17:32:18 +0100

    example chart

--- uu_isa_maing01-hi/src/routes/home.js
+++ uu_isa_maing01-hi/src/routes/home.js
@@ -47,74 +47,6 @@ export const Home = createVisualComponent({
   render(props) {
     //@@viewOn:private
 
-    let chartComponent = useRef();
-    let chartData = useState(null);
-    function onChartDidMount(chart) {
-      console.log("Chart : ", chart);
-      chartData = chart;
-    }
-
-    if (chartData != null) {
-      console.log(chartData);
-      useEffect(() => {
-        const chart = chartComponent.chart;
-        const am4core = chartComponent.am4core;
-        const am4charts = chartComponent.am4charts;
-
-        chart.data = [
-          {
-            country: "Lithuania",
-            litres: 501.9,
-          },
-          {
-            country: "Czech Republic",
-            litres: 301.9,
-          },
-          {
-            country: "Ireland",
-            litres: 201.1,
-          },
-          {
-            country: "Germany",
-            litres: 165.8,
-          },
-          {
-            country: "Australia",
-            litres: 139.9,
-          },
-          {
-            country: "Austria",
-            litres: 128.3,
-          },
-          {
-            country: "UK",
-            litres: 99,
-          },
-          {
-            country: "Belgium",
-            litres: 60,
-          },
-          {
-            country: "The Netherlands",
-            litres: 50,
-          },
-        ];
-
-        // Add and configure Series
-        let pieSeries = chart.series.push(new am4charts.PieSeries());
-        pieSeries.dataFields.value = "litres";
-        pieSeries.dataFields.category = "country";
-        pieSeries.slices.template.stroke = am4core.color("#fff");
-        pieSeries.slices.template.strokeWidth = 2;
-        pieSeries.slices.template.strokeOpacity = 1;
-
-        // This creates initial animation
-        pieSeries.hiddenState.properties.opacity = 1;
-        pieSeries.hiddenState.properties.endAngle = -90;
-        pieSeries.hiddenState.properties.startAngle = -90;
-      });
-    }
-
     //@@viewOff:private
 
     //@@viewOn:interface
@@ -125,20 +57,60 @@ export const Home = createVisualComponent({
 
     return (
       <div {...attrs}>
-        <UU5.Bricks.Row className={CLASS_NAMES.welcomeRow()}>
-          <UU5.Bricks.Column colWidth="x-12 s-3">
-            <Plus4U5.Bricks.UserPhoto width="100px" />
-          </UU5.Bricks.Column>
-          <UU5.Bricks.Column colWidth="x-12 s-9">
-            <UU5.Bricks.Header level="2" content={<UU5.Bricks.Lsi lsi={Lsi.auth.welcome} />} />
-            <UU5.Common.Identity>
-              {({ identity }) => <UU5.Bricks.Header level="2" content={identity.name} />}
-            </UU5.Common.Identity>
-          </UU5.Bricks.Column>
-        </UU5.Bricks.Row>
-        <UU5.Bricks.Row className={CLASS_NAMES.welcomeRow()}>
-          <UU5.AmCharts.Chart type={"PieChart"} onMount={onChartDidMount} ref={chartComponent} />
-        </UU5.Bricks.Row>
+        <UU5.AmCharts.Chart
+          type="PieChart"
+          config={{
+            series: [
+              {
+                type: "PieSeries",
+                dataFields: {
+                  value: "litres",
+                  category: "country",
+                },
+              },
+            ],
+            data: [
+              {
+                country: "Lithuania",
+                litres: 501.9,
+              },
+              {
+                country: "Czech Republic",
+                litres: 301.9,
+              },
+              {
+                country: "Ireland",
+                litres: 201.1,
+              },
+              {
+                country: "Germany",
+                litres: 165.8,
+              },
+              {
+                country: "Australia",
+                litres: 139.9,
+              },
+              {
+                country: "Austria",
+                litres: 128.3,
+              },
+              {
+                country: "UK",
+                litres: 99,
+              },
+              {
+                country: "Belgium",
+                litres: 60,
+              },
+              {
+                country: "The Netherlands",
+                litres: 50,
+              },
+            ],
+            legend: {},
+          }}
+          height="512px"
+        />
       </div>
     );
     //@@viewOff:render
